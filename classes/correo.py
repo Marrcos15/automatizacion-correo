1c5d7b3350142780b5add4e1db666f8dde0e185c
Marrcos15 <mgonzalez.trabajo.18@gmail.com>
2025-01-11 15:47:43 +0100

    Add new feature: Organizar el correo con etiquetas

--- classes/correo.py
+++ classes/correo.py
@@ -7,55 +7,150 @@ from typing import Tuple, List
 from classes.logger import Logger
 
 
-
-
-# Usar el Logger para registrar los eventos
-logger = Logger("automatizacion_correo")
-
-
-def cargar_configuracion(ruta: str) -> configparser.ConfigParser:
-    """
-    Carga y valida el archivo de configuración.
-    """
-    config = configparser.ConfigParser()
-    config.read(ruta)
-
-    if not config.has_section("login") or \
-       not all(key in config["login"] for key in ["username", "password", "imap_server"]):
-        logger.error("Archivo de configuracion incompleto o inexistente.")
-        raise ValueError("Archivo de configuracion incompleto o inexistente.")
-
-    return config
-
 class Correo:
     def __init__(self):
-        self.config = cargar_configuracion("config.ini")
+        self.logger = Logger("automatizacion_correo")
+        self.config = self.cargar_configuracion("config.ini")
         self.username = self.config["login"]["username"].strip()
         self.password = self.config["login"]["password"].strip()
         self.imap_server = self.config["login"]["imap_server"].strip()
         self.imap = self.conectar_al_correo()
+        
+        
+        #Etiquetas
+        self.arbol_etiquetas = {
+            'banco' : ['openbank', 'santander'],
+            'busqueda-de-trabajo': ['infojobs', 'linkedin', 'tecnoempleo'],
+            'plataformas-juegos' : ['steam', 'ea', 'chess.com'],
+            'plataformas-programacion' : ['github', 'google', 'make', 'notion'],
+            'pruebas': [],
+            'redes-sociales' : ['facebook', 'instagram'],
+            'trabajo': ['nominas'],
+            'plataformas': ['netflix','spotify']
+        }
+        self.filtro_etiquetas = {
+            'openbank': {
+                'remitente': 'openbank',
+                'asunto': '',
+                'cuerpo': '',
+            },
+            'santander': {
+                'remitente': 'santander',
+                'asunto': '',
+                'cuerpo': '',
+            },
+            'infojobs': {
+                'remitente': 'infojobs',
+                'asunto': '',
+                'cuerpo': '',
+            },
+            'linkedin': {
+                'remitente': 'linkedin',
+                'asunto': '',
+                'cuerpo': '',
+            },
+            'tecnoempleo': {
+                'remitente': 'tecnoempleo',
+                'asunto': '',
+                'cuerpo': '',
+            },
+            'steam': {
+                'remitente': 'steam',
+                'asunto': '',
+                'cuerpo': '',
+            },
+            'ea': {
+                'remitente': 'ea',
+                'asunto': '',
+                'cuerpo': '',
+            },
+            'chess.com': {
+                'remitente': 'chess.com',
+                'asunto': '',
+                'cuerpo': '',
+            },
+            'github': {
+                'remitente': 'github',
+                'asunto': '',
+                'cuerpo': '',
+            },
+            'google': {
+                'remitente': 'google',
+                'asunto': '',
+                'cuerpo': '',
+            },
+            'make': {
+                'remitente': 'make',
+                'asunto': '',
+                'cuerpo': '',
+            },
+            'notion': {
+                'remitente': 'notion',
+                'asunto': '',
+                'cuerpo': '',
+            },
+            'nominas': {
+                'remitente': 'nominas',
+                'asunto': '',
+                'cuerpo': '',
+            },
+            'facebook': {
+                'remitente': 'facebook',
+                'asunto': '',
+                'cuerpo': '',
+            },
+            'instagram': {
+                'remitente': 'instagram',
+                'asunto': '',
+                'cuerpo': '',
+            },
+            'netflix': {
+                'remitente': 'netflix',
+                'asunto': '',
+                'cuerpo': '',
+            },
+            'spotify': {
+                'remitente': 'spotify',
+                'asunto': '',
+                'cuerpo': '',
+            }
+        }
+        
+    def cargar_configuracion(self, ruta: str) -> configparser.ConfigParser:
+        """
+        Carga y valida el archivo de configuración.
+        """
+        config = configparser.ConfigParser()
+        config.read(ruta)
+
+        if not config.has_section("login") or \
+           not all(key in config["login"] for key in ["username", "password", "imap_server"]):
+            self.logger.error("Archivo de configuracion incompleto o inexistente.")
+            raise ValueError("Archivo de configuracion incompleto o inexistente.")
+
+        return config
 
 
     def conectar_al_correo(self) -> object:
         #Conectar al servidor IMAP
         try:
             imap = imaplib.IMAP4_SSL(self.imap_server)
-            logger.log("Conexion al servidor IMAP establecida.")
+            self.logger.log("Conexion al servidor IMAP establecida.")
         except socket.gaierror as e:
-            logger.error(f"Error al resolver el servidor IMAP: {self.imap_server}. Detalles: {e}")
+            self.logger.error(f"Error al resolver el servidor IMAP: {self.imap_server}. Detalles: {e}")
             raise ValueError(f"Error al resolver el servidor IMAP: {self.imap_server}. Detalles: {e}")
         except Exception as e:
-            logger.error(f"Error al inicializar la conexion IMAP: {e}")
+            self.logger.error(f"Error al inicializar la conexion IMAP: {e}")
             raise ValueError(f"Error al inicializar la conexion IMAP: {e}")
 
         #Autenticacion al correo
         try:
             imap.login(self.username, self.password)
-            logger.log("Autenticacion exitosa.")               
+            self.logger.log("Autenticacion exitosa.")               
         except imaplib.IMAP4.error as e:
-            logger.error(f"Error de autenticacion o conexion: {e}")
+            self.logger.error(f"Error de autenticacion o conexion: {e}")
         except Exception as e:
-            logger.error(f"Error inesperado: {e}")
+            self.logger.error(f"Error inesperado: {e}")
 
         return imap
 
@@ -63,16 +158,16 @@ class Correo:
         """
         Cierra la conexión al servidor de correo de forma segura.
         """
-        logger.log("Desconectando del servidor de correo.")
+        self.logger.log("Desconectando del servidor de correo.")
         try:
             if self.imap is not None:
                 # Verifica si la conexión sigue activa
                 if self.imap.state == 'SELECTED' or self.imap.state == 'AUTH':
                     self.imap.logout()
                 else:
-                    logger.log("La conexión IMAP ya estaba cerrada.")
+                    self.logger.log("La conexión IMAP ya estaba cerrada.")
         except Exception as e:
-            logger.error(f"Error al intentar desconectar: {e}")
+            self.logger.error(f"Error al intentar desconectar: {e}")
 
     def seleccionar_bandeja(self, bandeja: str) -> object:
         """
@@ -80,27 +175,29 @@ class Correo:
         """
         status, mensajes = self.imap.select(bandeja)
         if status != "OK":
-            logger.error(f"Error al seleccionar la bandeja de correo: {bandeja}. Detalles: {mensajes}")
+            self.logger.error(f"Error al seleccionar la bandeja de correo: {bandeja}. Detalles: {mensajes}")
         else:
-            logger.log(f"Bandeja de correo seleccionada: {bandeja}")
+            self.logger.log(f"Bandeja de correo seleccionada: {bandeja}")
 
         return self.imap
 
-    def filtrar_correo(self, etiqueta: str) -> list:
+    def filtrar_correo(self, filtro: str) -> list:
         """
         Filtra correos según la etiqueta proporcionada.
         """
         try:
-            status, mensajes = self.imap.search(None, etiqueta)
+            status, mensajes = self.imap.search(None, filtro)
             if status != "OK":
-                logger.error(f"Error al buscar correos con la etiqueta '{etiqueta}'.")
-                return "ERROR", []
+                self.logger.error(f"Error al buscar correos con la etiqueta '{filtro}'.")
+                return []
         except Exception as e:
-            logger.error(f"Error al buscar correos con la etiqueta '{etiqueta}'. Detalles: {e}")
-            return "ERROR", []
+            self.logger.error(f"Error al buscar correos con la etiqueta '{filtro}'. Detalles: {e}")
+            return []
 
-        logger.log(f"Total de correos {etiqueta}: {len(mensajes[0].split())}")
-        return mensajes
+        self.logger.log(f"Total de correos {filtro}: {len(mensajes[0].split())}")
+        list_idmensajes = mensajes[0].split()
+
+        return list_idmensajes
     
     def decodificar_correos(self, mensaje: Tuple) -> Tuple[str, str, str]:
         """
@@ -119,13 +216,13 @@ class Correo:
                 try:
                     subject = subject.decode(encoding or "latin1")
                 except Exception as e:
-                    logger.warning(f"Error al decodificar el asunto: {e}")
+                    self.logger.warning(f"Error al decodificar el asunto: {e}")
                     subject = "Error al decodificar el asunto"
             elif isinstance(subject, str):
                 # Si el asunto ya es de tipo string, no necesitamos decodificarlo
                 pass
             else:
-                logger.warning("El asunto tiene un formato inesperado.")
+                self.logger.warning("El asunto tiene un formato inesperado.")
                 subject = "Asunto no disponible"
 
             # Obtener el remitente y decodificarlo si es necesario
@@ -134,7 +231,7 @@ class Correo:
                 try:
                     from_ = from_.decode(encoding or "latin1")
                 except Exception as e:
-                    logger.error(f"Error al decodificar el remitente: {e}")
+                    self.logger.error(f"Error al decodificar el remitente: {e}")
                     from_ = "Error al decodificar el remitente"
 
             # Inicializamos 'body' como None para manejarlo más adelante
@@ -152,7 +249,7 @@ class Correo:
                             body = part.get_payload(decode=True).decode("latin1", errors="replace")
                             break  # Salimos del bucle al encontrar el HTML
                         except Exception as e:
-                            logger.warning(f"Error al decodificar el cuerpo HTML. Detalles: {e}")
+                            self.logger.warning(f"Error al decodificar el cuerpo HTML. Detalles: {e}")
 
                     # Si encontramos texto plano solo después de no haber encontrado HTML, lo procesamos
                     elif content_type == "text/plain":
@@ -160,7 +257,7 @@ class Correo:
                             body = part.get_payload(decode=True).decode("latin1", errors="replace")
                             break
                         except Exception as e:
-                            logger.warning(f"Error al decodificar el cuerpo de texto. Detalles: {e}")
+                            self.logger.warning(f"Error al decodificar el cuerpo de texto. Detalles: {e}")
 
             else:
                 # Si no es multipart, simplemente decodificamos el cuerpo
@@ -168,14 +265,13 @@ class Correo:
                     body = msg.get_payload(decode=True).decode("latin1", errors="replace")
                     
                 except Exception as e:
-                    logger.error(f"Error al decodificar el cuerpo del mensaje. Detalles: {e}")
+                    self.logger.error(f"Error al decodificar el cuerpo del mensaje. Detalles: {e}")
             
             correo = {
                 "asunto": subject,
                 "remitente": from_,
                 "cuerpo": body
             }
-            logger.log(f"Correo decodificado: {correo}")
             return correo
 
     def obtener_correos(self, mensajes: list) -> List[dict]:
@@ -183,16 +279,15 @@ class Correo:
         Obtiene los correos sin leer.
         """
         todos_los_mensajes = {}
-        mensajes = mensajes[0].split()
+
         for num in mensajes:
             status, mensaje_data = self.imap.fetch(num, "(BODY.PEEK[])")
             if status == "OK":
-                logger.log(f"Obteniendo correo sin leer: {num.decode('utf-8')}")
                 dict_correo = self.decodificar_correos(mensaje_data[0])
-                
+                # Agregar el correo decodificado al diccionario
                 todos_los_mensajes[num.decode('utf-8')] = dict_correo
             else:
-                logger.error(f"Error al obtener el correo sin leer. Detalles: {mensaje_data}")
+                self.logger.error(f"Error al obtener el correo sin leer. Detalles: {mensaje_data}")
 
         return todos_los_mensajes
             
@@ -205,9 +300,158 @@ class Correo:
 
         # Obtener los correos sin leer
         mensajes = self.filtrar_correo("UNSEEN")
-        
+
         no_leidos = self.obtener_correos(mensajes)
-        
-        self.desconectar_del_correo()
-        
         return no_leidos
+    
+    def listar_etiquetas(self) -> List[str]:
+        """
+        Lista todas las etiquetas (carpetas) disponibles en la cuenta IMAP.
+
+        :param imap: Objeto de conexión IMAP.
+        :return: Lista de etiquetas con sus nombres y detalles.
+        """
+        try:
+            status, folders = self.imap.list()
+            if status != "OK":
+                self.logger.warning("No se pudieron obtener las etiquetas.")
+                return []
+
+            etiquetas = []
+            for folder in folders:
+                # Decodificar la respuesta
+                parts = folder.decode().split(' "/" ')
+                if len(parts) == 2:
+                    etiquetas.append(parts[1].strip('"'))  # Nombre de la etiqueta
+
+            return etiquetas
+    
+        except Exception as e:
+            print(f"Error al listar etiquetas: {e}")
+            return []
+    
+    def eliminar_correos(self, mensajes: list) -> bool:
+        """
+        Elimina los correos proporcionados.
+        """
+        for num in mensajes:
+            status, _ = self.imap.store(num, '+FLAGS', '\\Deleted')
+            if status != "OK":
+                self.logger.error(f"No se pudo eliminar el mensaje {num}")
+
+        # Expurgar (eliminar físicamente) los mensajes marcados como eliminados
+        self.imap.expunge()
+        return True
+    
+    
+    def mover_correos(self, origen: str, destino: str, id_mensajes: list) -> None:
+        """
+        Mueve correos de una etiqueta a otra.
+
+        :param origen: Nombre de la etiqueta (carpeta) origen.
+        :param destino: Nombre de la etiqueta (carpeta) destino.
+        :param id_mensajes: Lista de IDs de mensajes a mover.
+        """
+        try:
+            # Obtener etiquetas
+            etiquetas = self.listar_etiquetas()
+
+            # Verificar que la etiqueta destino existe
+            destino_utf7 = destino.encode('utf-7').decode('ascii')
+            if destino_utf7 not in etiquetas:
+                self.logger.error(f"La carpeta destino '{destino}' no existe.")
+                return
+
+            # Seleccionar la carpeta origen
+            status, _ = self.imap.select(origen)
+            if status != "OK":
+                self.logger.error(f"No se pudo seleccionar la carpeta '{origen}'")
+                return
+
+            self.logger.log(f"Se seleccionó la carpeta '{origen}'")
+            
+            # Mover los mensajes a la carpeta destino
+            for mensaje_id in id_mensajes:
+                self.logger.debug(f"Intentando copiar mensaje {mensaje_id} a '{destino_utf7}'")
+
+                status, response = self.imap.copy(mensaje_id, destino)
+                if status != "OK":
+                    self.logger.warning(f"No se pudo copiar el mensaje {mensaje_id} a '{destino}'")
+                    continue
+
+                self.logger.debug(f"Se copió el mensaje {mensaje_id} a '{destino}'")
+
+            # Eliminar los mensajes de la carpeta origen
+            if self.eliminar_correos(id_mensajes):
+                # Eliminar el mensaje de la carpeta origen
+                self.logger.log(f"Se eliminó los mensajes {id_mensajes} de '{origen}'")
+            else:
+                self.logger.warning(f"No se pudieron eliminar los mensajes {id_mensajes} de '{origen}'")
+
+        except Exception as e:
+            self.logger.warning(f"Error al mover correos: {e}")
+                
+    def marcar_como_no_leidos(self, id_mensajes: list) -> None:
+        """
+        Marca los mensajes como no leídos.
+
+        :param id_mensajes: Lista de IDs de mensajes a marcar como no leidos.
+        """
+        for mensaje_id in id_mensajes:
+            # Marcar el mensaje como no leído quitando la bandera '\Seen'
+            status, _ = self.imap.store(mensaje_id, '-FLAGS', '\\Seen')
+            if status != "OK":
+                self.logger.error(f"No se pudo marcar como no leído el mensaje {mensaje_id}")
+            else:
+                self.logger.debug(f"Se marcó como no leído el mensaje {mensaje_id}")
+                
+            
+        
+            
+    def crear_diccionario_filtros(self) -> dict:
+        """
+        Crea un diccionario con el filtro de cada etiqueta hija.
+
+        :param arbol_etiquetas: Diccionario del árbol de etiquetas.
+        :param filtro_etiquetas: Diccionario de filtros para las etiquetas.
+        :return: Diccionario con los filtros de cada etiqueta hija.
+        """
+        diccionario_filtros = {}
+
+        for etiqueta_padre, etiquetas_hijas in self.arbol_etiquetas.items():
+            for etiqueta_hija in etiquetas_hijas:
+                
+                filtro = ""
+                if etiqueta_hija not in self.filtro_etiquetas:
+                    self.logger.warning(f"No se encontró el filtro para la etiqueta '{etiqueta_hija}'")
+                else:
+                    self.logger.debug(f"Filtro encontrado para la etiqueta '{etiqueta_hija}'")
+                    filtros_etiqueta = self.filtro_etiquetas[etiqueta_hija]
+                    
+                    # Crear el filtro para la etiqueta hija
+                    if filtros_etiqueta["remitente"]:
+                        filtro += f"FROM {filtros_etiqueta['remitente']} "
+                    
+                    if filtros_etiqueta["asunto"]:
+                        filtro += f"SUBJECT {filtros_etiqueta['asunto']} "
+                    
+                    if filtros_etiqueta["cuerpo"]:
+                        filtro += f"BODY {filtros_etiqueta['cuerpo']} "
+                
+                if etiqueta_hija in self.filtro_etiquetas:
+                    diccionario_filtros[f"{etiqueta_padre}/{etiqueta_hija}"] = filtro.strip()
+                    
+        return diccionario_filtros
+    
+    def total_mensajes(self) -> int:
+        """
+        Obtiene el total de mensajes en la bandeja de entrada.
+        """
+        status, mensajes = self.imap.search(None, "ALL")
+        if status != "OK":
+            self.logger.error("Error al obtener el total de mensajes.")
+            return 0
+
+        total_mensajes = len(mensajes[0].split())
+        self.logger.log(f"Total de mensajes en la bandeja de entrada: {total_mensajes}")
+        return total_mensajes
---
+++ organizar-correo.py
@@ -0,0 +1,72 @@
+from classes.correo import Correo
+from classes.logger import Logger
+
+
+# Conectar al servidor de correo
+correo = Correo() 
+
+#Obtener etiquetas y filtros del correo
+diccionario_filtros = correo.crear_diccionario_filtros()
+
+
+try:
+    #Obtener los mensajes del correo de la bandeja de entrada
+    correo.seleccionar_bandeja("INBOX")
+    
+    #Total de mensajes en la bandeja de entrada
+    total_mensajes = correo.total_mensajes()
+    
+    if total_mensajes == 0:
+        correo.logger.log("No hay mensajes en la bandeja de entrada")
+        raise Exception("No hay mensajes en la bandeja de entrada")
+        exit()
+        
+    mensajes_movidos = 0
+    
+    for etiqueta,filtro in diccionario_filtros.items():
+        correo.logger.log(f"Etiqueta: {etiqueta} - Filtro: {filtro}")
+        
+        #Obtener los mensajes con su filtro correspondiente
+        id_mensajes = correo.filtrar_correo(filtro)
+        #Si no hay mensajes con el filtro, se continua con el siguiente filtro
+        if len(id_mensajes) == 0:
+            correo.logger.log(f"No hay mensajes con el filtro {filtro}")
+            continue
+        #Mover los mensajes a la carpeta correspondiente
+        correo.mover_correos("INBOX", etiqueta, id_mensajes)
+        #Marcar los mensajes como no leidos
+        correo.marcar_como_no_leidos(id_mensajes)
+        mensajes_movidos += len(id_mensajes)
+        
+        correo.logger.log(f"Total de mensajes movidos: {mensajes_movidos} de {total_mensajes}")
+        
+    if mensajes_movidos != total_mensajes:
+        correo.logger.warning("No se movieron todos los mensajes")
+        raise Exception("No se movieron todos los mensajes")
+  
+except Exception as e:
+    correo.logger.error(f"Error: {e}")
+finally:
+    # Desconectar del servidor de correo
+    correo.desconectar_del_correo()
+    
+
+"""
+try:
+    # Obtener los mensajes del correo de la bandeja de entrada
+    mensajes = correo.obtener_todos_noleidos()
+    
+    for key in mensajes:
+        correo.logger.log(f"Asunto: {mensajes[key]['asunto']} - Remitente: {mensajes[key]['remitente']}")
+        #Etiquetar mensajes
+
+except Exception as e:
+    correo.logger.error(f"Error: {e}")
+
+finally:
+    # Desconectar del servidor de correo
+    correo.desconectar_del_correo()
+
+
+#correo.mover_correos("Inbox", r"Banco/Santander", "unseen")
+"""
